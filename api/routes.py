853b9acc1365e9771bf0a27a48a3e08591f6678d
gota_morishita <optimistic.gota@gmail.com>
2018-08-20 01:26:05 +0900

    fix typo

--- api/routes.py
+++ api/routes.py
@@ -1,13 +1,13 @@
 from api import api
 
-@bp.route('/hashtags/<str:hashtag>', method=['GET'])
+@api.route('/hashtags/<str:hashtag>', method=['GET'])
 def getByHashtags(hashtag):
     """
     Returns the list of tweets with a given hashtag in JSON format
     """
     pass
 
-@bp.route('/users/<str:user>', method=['GET'])
+@api.route('/users/<str:user>', method=['GET'])
 def getByUser(user):
     """
     Returns the list of tweets that certain user has on his/her own feed in JSON format
