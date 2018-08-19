5f0ca2bda3723c2d334b6622875f3cc37720f8a9
gota_morishita <optimistic.gota@gmail.com>
2018-08-20 02:59:05 +0900

    changed str to string to fix a bug

--- api/routes.py
+++ api/routes.py
@@ -1,13 +1,13 @@
 from api import api
 
-@api.route('/hashtags/<str:hashtag>', methods=['GET'])
+@api.route('/hashtags/<string:hashtag>', methods=['GET'])
 def getByHashtags(hashtag):
     """
     Returns the list of tweets with a given hashtag in JSON format
     """
     pass
 
-@api.route('/users/<str:user>', methods=['GET'])
+@api.route('/users/<string:user>', methods=['GET'])
 def getByUser(user):
     """
     Returns the list of tweets that certain user has on his/her own feed in JSON format
