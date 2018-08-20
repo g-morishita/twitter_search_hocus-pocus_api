49d4638b94b9d791f7c7e6dd6092e14ddc1804eb
gota_morishita <optimistic.gota@gmail.com>
2018-08-20 11:25:37 +0900

    implemented hastags' search

--- api/routes.py
+++ api/routes.py
@@ -1,11 +1,21 @@
-from api import api
+from api import api, tw_api
+from flask import jsonify
 
 @api.route('/hashtags/<string:hashtag>', methods=['GET'])
 def getByHashtags(hashtag):
     """
     Returns the list of tweets with a given hashtag in JSON format
     """
-    pass
+    
+    # get a response by a hashtag
+    hs_response = tw_api.request('search/tweets', { 'q': hashtag })
+
+    # convert TwitterResponse object to list of dict
+    hs_result = []
+    for tweet in hs_response:
+        hs_result.append(tweet)
+
+    return jsonify(hs_result)
 
 @api.route('/users/<string:user>', methods=['GET'])
 def getByUser(user):
